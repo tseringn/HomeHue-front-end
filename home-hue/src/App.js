90ff8bd00a37a926eaa94c653c56fda506106630
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-24 09:43:09 -0400

    semantics getting started

--- home-hue/src/App.js
+++ home-hue/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import HomeContainer from './containers/HomeContainer'
+import Login from './components/Login'
 
 function App() {
   return (
-  <HomeContainer/>
-  );
+    <Router>
+      <div>
+        <Route exact path="/" render= {routerProps => <HomeContainer {...routerProps}/>} />
+        <Route exact path="/login" component={Login}/>
+      </div>
+    </Router>
+  )
+  
 }
 
 export default App;
--- home-hue/src/containers/HomeContainer.js
+++ home-hue/src/containers/HomeContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Card, Image } from 'semantic-ui-react'
 
 
 class HomeContainer extends React.Component{
@@ -16,7 +17,42 @@ class HomeContainer extends React.Component{
     render(){
         return(
             <div>
-            {this.state.rooms.map(room=><h1>{room.name}</h1>)}
+                <h1>Home Hue</h1>
+                <Button inverted color='teal'>
+                    Teal
+                </Button>
+                <Button animated='fade' color="red" onClick={()=>this.props.history.push('/login')}>
+                    <Button.Content visible>Login or Create Account</Button.Content>
+                    <Button.Content hidden>Start Hueing!</Button.Content>
+                </Button>
+                <h3>Recently Created Room Schemes:</h3>
+                {this.state.rooms.map(room=><h1>{room.name}</h1>)}
+                <Card.Group>
+                    <Card>
+                    <Card.Content>
+                        <Image
+                        floated='right'
+                        size='mini'
+                        src='/images/avatar/large/steve.jpg'
+                        />
+                        <Card.Header>Steve Sanders</Card.Header>
+                        <Card.Meta>Friends of Elliot</Card.Meta>
+                        <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className='ui two buttons'>
+                        <Button basic color='green'>
+                            Approve
+                        </Button>
+                        <Button basic color='red'>
+                            Decline
+                        </Button>
+                        </div>
+                    </Card.Content>
+                    </Card>
+                </Card.Group>
             </div>
         )
     }
