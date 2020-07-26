83972c3fa964de015f29a08c251e5a20f5a23e68
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-26 17:48:09 -0400

    user dashboard functionality + design

--- home-hue/src/containers/UserPage.js
+++ home-hue/src/containers/UserPage.js
@@ -6,19 +6,28 @@ import { Button, Card, Image, Icon } from 'semantic-ui-react'
 
 class UserPage extends React.Component{
 
+    getUsersRooms = () => {
+        return this.props.currentUser.rooms.map(room=>(<RoomCard key ={room.id} {...room} user={this.props.currentUser} />))
+    }
+
 
     render(){
         return(
             <div>
                 <div className="user-page-nav">
-                    
-                    <i onClick={()=>this.props.history.push('/')}  className="home basic icon huge user-page-icon"></i>
-                    
-                    <i  className="pencil icon huge user-page-icon" ></i>
-                    <i className="add sign icon huge user-page-icon"></i>
+                    <i onClick={()=>this.props.history.push('/')}  className="home basic icon big user-page-icon"></i>
+                    <i  className="pencil icon big user-page-icon" ></i>
+                    <i className="add sign icon big user-page-icon"></i>
                 </div>
-                
-                <h2>My Rooms</h2>
+                <div className="user-header-info">
+                    <img className="main-profile-picture" src={this.props.currentUser.image_url} alt={this.props.currentUser.name}/>
+                    <div>
+                        <h2>{this.props.currentUser.name}</h2>
+                        <small>@{this.props.currentUser.username}</small>
+                        <h5>{this.props.currentUser.email}</h5>
+                    </div> 
+                </div>
+                {this.getUsersRooms()}
             </div>
         )
     }
