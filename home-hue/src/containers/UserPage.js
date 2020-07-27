1d5a3f7042c4647598f51a591f7b934fb8590056
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-27 11:08:42 -0400

    user page functionality

--- home-hue/src/containers/UserPage.js
+++ home-hue/src/containers/UserPage.js
@@ -1,23 +1,109 @@
 import React from 'react';
 import RoomCard from '../components/RoomCard'
 
-import { Button, Card, Image, Icon } from 'semantic-ui-react'
+import { Button, Card, Image, Icon, Modal, Header } from 'semantic-ui-react'
 
 
 class UserPage extends React.Component{
+
+    state = { 
+        
+        modalOpen: false,
+        room: {
+            name: '',
+            description: '',
+            img_url: '',
+            pvt: false
+        }
+    
+    }
+
+    handleOpen = () => this.setState({ modalOpen: true })
+
+    handleClose = () => this.setState({ modalOpen: false })
 
     getUsersRooms = () => {
         return this.props.currentUser.rooms.map(room=>(<RoomCard key ={room.id} {...room} user={this.props.currentUser} />))
     }
 
+    handleChange = (e) => {
+        this.setState({room: {...this.state.room, [e.target.name]: e.target.value}})
+    }
+
+    handleCheck = (e) => {
+        this.setState({room: {...this.state.room, pvt: !this.state.room.pvt} })
+    }
+
+    newRoomForm = () => {
+
+        const {name, description, img_url,pvt} = this.state.room
+
+    return (<form  >
+            <div className="ui large form" id="login-form" >
+            <h2>Create a Room</h2>
+            <div className="two fields">
+            <div className="field">
+                {/* <label>Name</label> */}
+                <input onChange={this.handleChange} name='name' value={name} className = "input" placeholder="Room Name" type="text"/>
+            </div>
+            
+            </div>
+                <div className="two fields">
+                    <div className="field">
+                        {/* <label>Username</label> */}
+                        <textarea rows="3" onChange={this.handleChange}  name='description' value={description} className = "input" placeholder="Description"/>
+                    </div>
+                </div>
+
+                <div className="two fields">
+                    <div className="field">
+                        {/* <label>Email</label> */}
+                        <input onChange={this.handleChange}  name='img_url' value={img_url} className = "input" placeholder="Main Room Image URL Here" type="email"/>
+                    </div>
+                </div>
+
+                <div className="two fields"> 
+                    <div className="ui toggle checkbox">
+                        <input onChange={this.handleCheck} checked={pvt} type="checkbox" name="pvt"/>
+                        <label style={{color: "white"}}>Make your room private</label>
+                    </div>
+                </div>
+            
+        
+            <button className="ui submit grey button" type='submit'>Create Account</button>
+            <button className="ui submit grey button" onClick={this.handleClose}>Cancel</button>
+        </div>
+        </form>)
+    }
+
+    
+
+   roomFormModal = () => (
+    <Modal
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        basic
+        size='small'>
+    
+        <Header  />
+        <Modal.Content>
+        {this.newRoomForm()}
+        </Modal.Content>
+        <Modal.Actions>
+
+        </Modal.Actions>
+    </Modal>
+   )
+        
 
     render(){
         return(
             <div>
+                {this.roomFormModal()}
                 <div className="user-page-nav">
                     <i onClick={()=>this.props.history.push('/')}  className="home basic icon big user-page-icon"></i>
                     <i  className="pencil icon big user-page-icon" ></i>
-                    <i className="add sign icon big user-page-icon"></i>
+                    <i onClick={this.handleOpen} className="add sign icon big user-page-icon"></i>
                 </div>
                 <div className="user-header-info">
                     <img className="main-profile-picture" src={this.props.currentUser.image_url} alt={this.props.currentUser.name}/>
@@ -28,6 +114,7 @@ class UserPage extends React.Component{
                     </div> 
                 </div>
                 {this.getUsersRooms()}
+                
             </div>
         )
     }
